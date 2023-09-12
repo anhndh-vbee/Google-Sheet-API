c92bdccbbc55e9c4f509303ae49de59359abdfa8
anhndh-vbee <anhndh.vbee@gmail.com>
2023-09-12 14:05:54 +0700

    Function merge arr

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -15,10 +15,33 @@ function splitArrayBySize(array, size) {
   return result;
 }
 
-// arr = [ [ {key:val1, ... }, {key:val2, ... } ], [...], [...]] => [val1, val2, ...]
+/**
+ * @param {*} arr = [[ {key:val1, ... }, {key:val2, ... } ], [...], [...]]
+ * @returns [val1, val2, ...]
+ */
+
 function flatArrayToGetJiraID(arr) {
   const result = arr.flatMap((subArr) => subArr.map((item) => item["Jira ID"]));
   return result;
 }
 
-module.exports = { splitArrayBySize, flatArrayToGetJiraID };
+/**
+ *
+ * @param {*} originListStoryWithSubtask
+ * @param {*} newListSubTaskAfterCreated
+ * @returns
+ */
+
+function combineArray(originListStoryWithSubtask, newListSubTaskAfterCreated) {
+  const newListStoryWithSubtask = originListStoryWithSubtask.map(
+    (story, index) => ({
+      ...story,
+      task: newListSubTaskAfterCreated[index].map((subtask) => ({
+        ...subtask,
+      })),
+    })
+  );
+  return newListStoryWithSubtask;
+}
+
+module.exports = { splitArrayBySize, flatArrayToGetJiraID, combineArray };
